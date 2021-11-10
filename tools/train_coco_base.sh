d988f8a12977d3d7a0caabe4d1a5a1a1b7b1b703
zhanglibin.buaa <zhanglibin.buaa@bytedance.com>
2021-11-10 22:12:01 +0800

    mod train.sh

--- tools/train_coco_base.sh
+++ tools/train_coco_base.sh
@@ -27,14 +27,27 @@ GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 configs/r50_deformable_detr_plus_it
 
 
 # finue-tune 
-
+80 class :
 GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 configs/r50_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh   \
+    --lr_backbone 0 \
     --num_classes 80     \
     --dataset_name coco_all_seed_0_10_shot     \
     --eval_dataset coco_all   \
-    --output_dir exps/coco_novel_100_q_resnet50_seed0_10shot     \
+    --output_dir exps/coco_all_100_q_resnet50_seed0_10shot     \
     --batch_size 4 \
     --resume surgery_model/checkpoint0049_base_441_100q.pth \
+    >> nohup_coco_base_1105_seed0_1shot.out 2>&1 &
+
+20 class :
+GPUS_PER_NODE=8 ./tools/run_dist_launch.sh 8 configs/r50_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh   \
+    --lr_backbone 0 \
+    --num_classes 20    \
+    --filter_kind novel \
+    --dataset_name coco_all_seed_0_10_shot     \
+    --eval_dataset coco_novel   \
+    --output_dir exps/coco_novel_100_q_resnet50_seed0_10shot     \
+    --batch_size 4 \
+    --resume surgery_model/checkpoint0049_20_classes_resnet50_100q.pth \
     >> nohup_coco_base_1105_seed0_1shot.out 2>&1 &
 
 GPUS_PER_NODE=8  ./tools/run_dist_launch.sh 8 configs/r50_deformable_detr_plus_iterative_bbox_refinement_plus_plus_two_stage.sh \
