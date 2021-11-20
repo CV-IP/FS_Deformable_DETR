dfcd6236632b1d71cc284af06d5af7063a92b781
zhanglibin.buaa <zhanglibin.buaa@bytedance.com>
2021-11-20 16:07:58 +0800

    add one stage novel res

--- tools/analyse_res.py
+++ tools/analyse_res.py
@@ -4,19 +4,20 @@ from glob import glob
 
 
 
-log_dir = '/opt/tiger/minist/FS_Deformable_DETR/log/'
+log_dir = '/opt/tiger/minist/FS_Deformable_DETR/log/coco_novel_resnet50_q100_onestage_bboxRefine'
 res_file = os.path.join(log_dir, 'res.json')
 
 
 
 seeds = list(range(10))
-shots = [2, 3, 5, 10, 30]
+# shots = [2, 3, 5, 10, 30]
+shots = [10, 30]
 res_f = open(res_file, 'w') # have to close this file
 for shot in shots :
     total_mAP = 0.0
     mAPs = []
     for seed in seeds:
-        log_file = 'coco_novel_seed{}_{}shot.log'.format(seed, shot)
+        log_file = 'seed{}_{}shot.log'.format(seed, shot)
         f = open(os.path.join(log_dir, log_file), 'r')
         line = f.readlines()[-1].strip()
         f.close()
